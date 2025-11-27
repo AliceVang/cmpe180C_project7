6858e3d79a014275b0ff21cf05950bbd3972a3cd
Alice Vang <alicevang18@gmail.com>
2025-11-27 23:33:47 +0000

    initial commit

---
+++ producer-consumer.c
@@ -0,0 +1,37 @@
+// lab7: producer-consumer.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+
+int main (int argc, char *argv[]){
+	int opt;
+	while((opt = getopt(argc,argv, "p:c:m:q:u:s:")) != -1){
+		switch(opt){
+			case'p':	// producer
+				printf("case p\n");
+				break;
+			case 'c':	// consumer
+				printf("case c\n");
+				break;
+			case 'm':	// message produced by producer
+				printf("case m\n");
+				break;
+			case 'q':	// depth of the queue
+				printf("case q\n");
+				break;
+			case 'u':	// unit socket
+				printf("case u\n");
+				break;
+			case 's':	// shared memory
+				printf("case s\n");
+				break;
+			default:
+				fprintf(stderr, "Error: Unknown option.\n");
+				exit(1);
+		}
+	}
+
+	return 0;
+}
