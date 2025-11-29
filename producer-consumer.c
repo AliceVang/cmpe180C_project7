9b80b672aba7aa2310589a6412309a4f261b6e0e
Alice Vang <alicevang18@gmail.com>
2025-11-29 07:54:24 +0000

    ok

--- producer-consumer.c
+++ producer-consumer.c
@@ -4,37 +4,81 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <string.h>
+#include <semaphore.h>
+
+typedef enum {MODE_NONE, MODE_PRODUCER, MODE_CONSUMER} mode_type;
+typedef enum {IPC_NONE, IPC_UNIX_SOCKET, IPC_SHARED_MEMORY} ipc_t;
+typedef struct{
+	int capacity;
+	int in;
+	int out;
+	sem_t mutex;
+	sem_t empty;
+	sem_t full;
+	//char buff[][];
+} shared_memory_queue_t;
+sem_t *sem_full = NULL;
+sem_t *sem_empty = NULL;
+sem_t *sem_mutex = NULL;
+
+static int e_status = 0;
+
 
 int main (int argc, char *argv[]){
+	mode_type mode = MODE_NONE;
+	ipc_t ipc = IPC_NONE;
+	char *message = NULL;
+	int queue_depth = 0;
+
 	int opt;
 	while((opt = getopt(argc,argv, "pcm:q:use")) != -1){
 		switch(opt){
 			case'p':	// producer
-				printf("case p\n");
+				mode = MODE_PRODUCER;
 				break;
 			case 'c':	// consumer
-				printf("case c\n");
+				mode = MODE_CONSUMER;
 				break;
 			case 'm':	// message produced by producer
-				printf("case m\n");
+				message = optarg;
 				break;
 			case 'q':	// depth of the queue
-				printf("case q\n");
+				queue_depth = atoi(optarg);
 				break;
 			case 'u':	// unit socket
-				printf("case u\n");
+				ipc = IPC_UNIX_SOCKET;
 				break;
 			case 's':	// shared memory
-				printf("case s\n");
+				ipc = IPC_SHARED_MEMORY;
 				break;
 			case 'e':	// print the string being produced/consumed + \n
-				printf("case e\n");
+				e_status = 1;
 				break;
 			default:
 				fprintf(stderr, "Error: Unknown option.\n");
 				exit(1);
 		}
 	}
+	if(mode == MODE_PRODUCER && !message){
+		fprintf(stderr, "Error: producer needs a message.\n");
+		return 1;
+	}
+
+	if(ipc == IPC_SHARED_MEMORY){
+		if(mode == MODE_PRODUCER){
+			// run producer in shared memory
+
+		} else if(mode == MODE_CONSUMER) {
+			// run consumer in shared memory
+		}
+	} else if(ipc = IPC_UNIX_SOCKET){
+		if(mode == MODE_PRODUCER){
+			// run producer in unix socket
+		} else if(mode == MODE_CONSUMER){
+			// run consumer in unix socket
+		}
+	}
+
 
 	return 0;
 }
